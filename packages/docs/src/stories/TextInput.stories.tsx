dbda43800cfb9441bf42b9a8d533fcd9af1e959e
guivicentep <guivicente.gvp@hotmail.com>
2023-08-28 22:39:29 -0300

    feat: Criação do componente TextArea

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Box, Text, TextInput, TextInputProps } from '@gvp-ui/react'
 
 export default {
-  title: 'Form/TextInput',
+  title: 'Form/Text Input',
   component: TextInput,
   args: {},
   decorators: [
